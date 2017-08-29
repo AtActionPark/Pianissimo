4532613b364bdbf70baa7d57765fa57a2e622ceb
clement matheron <clement.matheron@gmail.com>
2017-08-29 11:21:43 +0200

    Fix modules

--- index.js
+++ index.js
@@ -12,24 +12,39 @@ function noteConstructor(name){
 function intervalConstructor(arg1,arg2){
     return new Interval(arg1,arg2)
 }
+function getRandomNote(octave1,octave2){
+    let note =  Helpers.pickRandomArray(Theory.fullNotesList)
+    let octave = Helpers.getRandomInt(octave1,octave2)
+  
+    return new Note(note+octave)
+}
+function getRandomInterval(){
+    let intervalName =  Helpers.pickRandomProperty(Theory.intervalsDict)
+    let intervalOrder = Math.random()<0.5? 'ascending' : 'descending'
+  
+    return new Interval(intervalName, intervalOrder)
+}
 
 let solfege = {
     note:noteConstructor,
     interval:intervalConstructor,
+    randomNote:getRandomNote,
+    randomInterval:getRandomInterval,
     Note:Note,
     Interval:Interval,
     Chord:Chord,
 }
 exports = module.exports = solfege
 
-let note = solfege.note('C4')
-let interval = solfege.interval('m3')
-
+let note = solfege.randomNote(3,4)
+let interval = solfege.randomInterval()
 let result = note.plusInterval(interval)
+let computedInterval = solfege.interval(note,result)
 
-console.log(note.name)
-console.log(interval.name)
-console.log(result.name)
+//console.log('Root note: ' + note.name)
+//console.log('Interval :' + interval.order + ' ' + interval.name)
+//console.log('Computed Interval :' + computedInterval.order + ' ' + computedInterval.name)
+//console.log('Result note: ' + result.name)
 
 
 
--- test/test.js
+++ test/test.js
@@ -1,22 +1,15 @@
 'use strict';
-var Note = require('../lib/note');
-var Interval = require('../lib/interval');
-var Chord = require('../lib/chord');
-var Helpers = require('../lib/helper');
-var Theory = require('../lib/theory');
+var solfege = require('../index');
 
 QUnit.test( "Interval to notes", function( assert ) {
-
     for(let i = 0;i<100;i++){
-        let note = Helpers.getRandomNoteFull(3,4) 
-        let order = Math.random()<0.5 ? 'ascending':'descending'
-        let intervalName = Helpers.getRandomInterval()
-        let interval = new Interval(intervalName, order)
+        let note = solfege.randomNote(3,4)
+        let interval = solfege.randomInterval()
+        let targetNote = note.plusInterval(interval)
         
-        let targetNote = note.getNoteFromInterval(interval)
         if(targetNote == 'impossible to compute')
             continue
-        let computedInterval = s.getIntervalFromNotes(note,targetNote)
+        let computedInterval = solfege.interval(note,targetNote)
         if(computedInterval.quality == '$')
             continue
         
@@ -24,20 +17,19 @@ QUnit.test( "Interval to notes", function( assert ) {
         assert.equal( interval.semitones ,computedInterval.semitones, "rootNote: " + note.name + '-' + targetNote.name + ' -  intervalNameBasic: '+ interval.name + ' -  intervalNameComputed: '+ computedInterval.name);
     } 
 });
-/* QUnit.test( "Notes to Interval", function( assert ) {
-    let s = new Solfege()
+QUnit.test( "Notes to Interval", function( assert ) {
     for(let i = 0;i<100;i++){
-        let note1 = s.getRandomNoteFull(3,4) 
-        let note2 = s.getRandomNoteFull(3,4) 
+        let note1 = solfege.randomNote(3,4)
+        let note2 = solfege.randomNote(3,4)
 
-        let interval = s.getIntervalFromNotes(note1,note2)
+        let interval = solfege.interval(note1,note2)
 
-        let note = s.getNoteFromInterval(note1, interval)
+        let note = note1.plusInterval(interval)
         if(note == 'impossible to compute' ) continue
 
         assert.equal(note.name, note2.name,'Notes: '+note1.name+'-'+note2.name+' - Interval: ' + interval.name )
     } 
-});  */
+});  
 
 
 
